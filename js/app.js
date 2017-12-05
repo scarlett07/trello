0a687a869e3a0211a63f40fec24b924d8e9f7942
Behtsa <monze_gdbm@hotmail.com>
2017-12-04 20:40:10 -0600

    Version 0.0.3.0

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 //global variables
 var listContainer = document.createElement('div');
-
-
 //events
 document.getElementById('title_list').addEventListener('click',showButtons);
 document.getElementById('button2').addEventListener('click', hideButtons);
 document.getElementById('button1').addEventListener('click',addNewList);
 document.getElementById('add_card').addEventListener('click',showTextArea);
+
 //functions
 function showButtons() {
   document.getElementById('button1').style.display = 'block';
@@ -39,6 +38,17 @@ function addNewList() {
 
 function showTextArea() {
   var toDo = document.createElement('textarea');
+  var add = document.createElement('button');
+  var close = document.createElement('button');
+  var pNewList = document.createElement('p');
   toDo.className = 'textarea';
+  add.innerHTML = 'Add';
+  close.innerText = 'X';
   listContainer.appendChild(toDo);
+  listContainer.appendChild(add);
+  listContainer.appendChild(close);
 }
+
+function createCard() {
+}
+
