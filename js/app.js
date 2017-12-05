923a0c6971be5bd7f0341cdc680e72e52e5712b6
Behtsa <monze_gdbm@hotmail.com>
2017-12-04 18:03:15 -0600

    Version 0.0.0

--- js/app.js
+++ js/app.js
@@ -1,36 +1,12 @@
+document.getElementById('title_list').addEventListener('click',showButtons);
+document.getElementById('button2').addEventListener('click', hideButtons);
 
-// Llamamos los elementos del HTML a JS
-var containerToDo = document.getElementById('container-to-do');
-// Creamos los elementos que necesitamos
-var title= document.createElement('h1');
-var form = document.createElement('form');
-var toDo = document.createElement('input');
-var btnSubmit = document.createElement('button');
+function showButtons() {
+  document.getElementById('button1').style.display = 'block';
+  document.getElementById('button2').style.display = 'block';
+}
 
-// Le agregamos atributos a esos elementos creados
-  toDo.setAttribute("type", "text");
-  btnSubmit.innerText = 'Guardar';
-  toDo.id = 'input';
-//  Decimos donde se van a crear
-containerToDo.appendChild(title);
-containerToDo.appendChild(form);
-form.appendChild(toDo);
-containerToDo.appendChild(btnSubmit);
-
-
-function showTitle() {
-  event.preventDefault();
-  // traemos los elementos
-  var titleToDo = document.getElementById('title').value;
-  var addToDo = document.createElement('a');
-  //atributos
-  title.innerText=titleToDo;
-  title.style.fontSize= 'medium';
-  title.style.textAlign='left';
-  addToDo.id= 'add';
-  addToDo.innerText = ('Añadir una tarjeta');
-  //eventos
-  addToDo.onclick= showToDoInContainer;
-  //Crear
-  containerToDo.appendChild(addToDo);
+function hideButtons() {
+  document.getElementById('button1').style.display = 'none';
+  document.getElementById('button2').style.display = 'none';
 }
